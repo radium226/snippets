761907f99abbaeb16df6a74cfc7665873fc10894
radium226 <adrien.besnard@gmail.com>
2014-01-09 08:45:29 +0100

    Add global Makefile

---
+++ C/hello-world/snippet.c
@@ -0,0 +1,11 @@
+#include "snippet.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int UNUSED(argc), char **UNUSED(argv))
+{
+	printf("%s, %s! \n", "Hello", "World");
+
+	exit(EXIT_SUCCESS);
+}
---
+++ C/hello-world/snippet.h
@@ -0,0 +1,3 @@
+#define UNUSED(x) UNUSED_ ## x __attribute__((__unused__))
+
+int main(int argc, char **argv);
